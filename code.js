59c7cd36596ea2e6cbd9e9645e101d96feef2bf2
Nicholas <nicholasdiazjr509@gmail.com>
2021-10-15 13:54:34 -0500

    commit

--- code.js
+++ code.js
@@ -1,4 +1,16 @@
-// helloWorld function 
+//A.K.A. solutions file.
+
+// helloWorld function
 function helloWorld() {
     return "Hello, World!";
+}
+
+function sayHello(input){
+   // return  "Hello"  + input;
+    return "Hello Nick";
+}
+
+function isFive(input){
+//return typeof input !== 'number';
+    return input == '5';
 }
--- tests.js
+++ tests.js
@@ -13,3 +13,41 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+describe('sayHello', function (){
+    it('should be defined as a function', function (){
+        expect(typeof sayHello).toBe('function');  //test for sayHello function
+    })
+    it('should never return undefined', function (){
+        expect(typeof sayHello()).not.toBe('undefined')
+    })
+    it('should return a string', function (){
+        expect(typeof sayHello()).toBe('string');
+
+    })
+    it('should return a greeting for the input', function (){
+        expect(sayHello('Nick')).toBe('Hello Nick');
+        //expect(sayHello('Bob')).toBe('Hello Bob');
+    })
+})
+// Exercise #10 Let's Test-Drive an isFive function!
+// Inside of tests.js, write a describe block for our new isFive function.
+// As your first, failing test, write an it and an expect asserting that a function named isFive exists.
+//    Run the tests by refreshing report.html to show the red, failing test.
+//    Write just enough code inside of code.js to define an empty function for isFive.
+//    Now, refresh report.html to ensure that all tests are green.
+//    What other tests and implementation cycles should you do for isFive?
+//    Ensure that isFive returns a boolean no matter what the input
+// Ensure that isFive returns true when passed 5
+// What about if we pass in the string "5"? Do you want isFive to return true for that?
+//    If so, write the test, ensure that the test is failing, and then write the implementation
+// Commit your work to git and push to GitHub before moving forward.
+describe('isFive', function () {
+    it('should return', function () {
+        expect(typeof isFive()).not.toBe('');
+    })
+    it('boolean', function (){
+                expect(typeof isFive()).toBe('boolean')
+
+    })
+})
+//Write a test for a function 'isTwo'.
